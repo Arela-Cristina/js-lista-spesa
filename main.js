ce244f9c4c9656b5fdbc4f014c09a8b4a98fa806
Arela-Cristina <instagrandine@gmail.com>
2024-06-12 21:28:52 +0200

    work done

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
 const arraySpesa = ['banana', 'fragola', 'mandorla', 'anguria', 'kiwi', 'mango'];
 
+let lista = document.getElementById('lista');
+
 let contatore = 0;
 while (contatore < arraySpesa.length) {
+    console.log(arraySpesa[contatore]);
+
+    let itemSpesa = document.createElement(`li`);
+    itemSpesa.append(arraySpesa[contatore]);
+    lista.append(itemSpesa);
+
     contatore++;
 }
 
-console.log(arraySpesa[contatore]);
-
-let lista = document.getElementsById ('lista');
-let itemSpesa = document.createElement('li')
