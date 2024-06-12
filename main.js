78d7aba89fd02e7c330bf1af80cf541338d9f509
Arela-Cristina <instagrandine@gmail.com>
2024-06-12 17:32:58 +0200

    Add array add ciclo while

---
+++ main.js
@@ -0,0 +1,8 @@
+const arraySpesa = ['banana', 'fragola', 'mandorla', 'anguria', 'kiwi', 'mango'];
+
+let contatore = 0;
+while (contatore < arraySpesa.length) {
+
+    console.log(arraySpesa);
+    contatore++;
+}
